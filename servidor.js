907b4e7e00eaef0e19d01a318705c2b711758041
Javico <javiconlinea@outlook.com>
2021-02-03 21:24:14 -0600

    cors deee

--- servidor.js
+++ servidor.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 
 
+// puerto de la app
+const PORT = process.env.PORT || 5000;
+
 const servidor = http.createServer(app);
 
 //Inicializamos socketio
@@ -38,4 +41,8 @@ io.on("connection", (socket) => {
   });
 });
 
-servidor.listen(5000, () => console.log("Servidor inicializado"));
+//servidor.listen(5000, () => console.log("Servidor inicializado"));
+// arrancar la app
+servidor.listen(PORT, () => {
+  console.log(`funcionando en el puerto ${PORT}`);
+});
